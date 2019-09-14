5f9fa2d95cbec9ff575f96d8789736fe3ca0a633
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2019-09-14 16:03:29 -0400

    fixed camera thingy

--- app/src/main/java/com/example/allergyapp/ui/VerificationOCRFragment/VerificationOcrViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.allergyapp.ui.VerificationOCRFragment;
-
-import androidx.lifecycle.ViewModel;
-
-public class VerificationOcrViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
