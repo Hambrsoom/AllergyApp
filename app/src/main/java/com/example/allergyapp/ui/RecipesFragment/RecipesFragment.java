e97cb8f8a6712fee3ec11c0e3dd2838e83bfc80e
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2019-09-14 08:05:08 -0400

    added some ui for recipe fragment

--- app/src/main/java/com/example/allergyapp/ui/RecipesFragment/RecipesFragment.java
+++ app/src/main/java/com/example/allergyapp/ui/RecipesFragment/RecipesFragment.java
@@ -1,13 +1,6 @@
 package com.example.allergyapp.ui.RecipesFragment;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +9,20 @@ import android.widget.TextView;
 
 import com.example.allergyapp.R;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class RecipesFragment extends Fragment {
 
     private RecipesViewModel mViewModel;
+    private RecyclerView recyclerView;
+    String[] items = {"item0","item1", "item3"};
+    RecyclerView.LayoutManager layoutManager;
+    RecipesAdapter mAdapter;
 
     public static RecipesFragment newInstance() {
         return new RecipesFragment();
@@ -28,6 +32,19 @@ public class RecipesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.recipes_fragment, container, false);
+        recyclerView = rootView.findViewById(R.id.recipesRecyclerView);
+        recyclerView.setAdapter(new RecipesAdapter(getActivity().getApplicationContext(), items));
+
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter
+        mAdapter = new RecipesAdapter(getActivity().getApplicationContext(), items);
+        recyclerView.setAdapter(mAdapter);
         return inflater.inflate(R.layout.recipes_fragment, container, false);
     }
 
