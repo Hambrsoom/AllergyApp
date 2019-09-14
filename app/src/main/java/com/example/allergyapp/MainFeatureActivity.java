eeab62bf33046b3a2c744bb3d0867312c574cf25
EdmundLuii <edmund.lui1998@gmail.com>
2019-09-14 12:01:21 -0400

    wtv

--- app/src/main/java/com/example/allergyapp/MainFeatureActivity.java
+++ app/src/main/java/com/example/allergyapp/MainFeatureActivity.java
@@ -1,9 +1,10 @@
 package com.example.allergyapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.allergyapp.ui.main.SectionsPagerAdapter;
@@ -16,7 +17,6 @@ import androidx.viewpager.widget.ViewPager;
 
 public class MainFeatureActivity extends AppCompatActivity {
 
-    private CheckBox check1, check2, check3, check4, check5, check6;
 
 
     @Override
@@ -39,31 +39,6 @@ public class MainFeatureActivity extends AppCompatActivity {
         });
     }
 
-    public void addListenerOnButton(View view) {
-        String s = "";
-        check1 = (CheckBox) findViewById(R.id.checkBox);
-        check2 = (CheckBox) findViewById(R.id.checkBox);
-        check3 = (CheckBox) findViewById(R.id.checkBox);
-        check4 = (CheckBox) findViewById(R.id.checkBox);
-        check5 = (CheckBox) findViewById(R.id.checkBox);
-        check6 = (CheckBox) findViewById(R.id.checkBox);
 
-        if (check1.isChecked()) {
-            s += "Dairy";
-        }if (check2.isChecked()) {
-            s += "Peanuts";
-        }if (check3.isChecked()) {
-            s += "Tree Nuts";
-        }if (check4.isChecked()) {
-            s += "Soy";
-        }if (check5.isChecked()) {
-            s += "Shellfish";
-        }if (check6.isChecked()) {
-            s += "Eggs";
-        }
-        s = s.substring(0,s.length()-1);
-        Toast.makeText(MainFeatureActivity.this,s, Toast.LENGTH_LONG).show();
-
-    }
 
 }
