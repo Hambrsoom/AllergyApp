2fc57c6d2f70cfe3c596bd7e05e03484c77e79f5
Hambrsoom <hampico1234@gmail.com>
2019-09-15 00:36:18 -0400

    working on verdict

--- app/src/main/java/com/example/allergyapp/VisualOCR.java
+++ app/src/main/java/com/example/allergyapp/VisualOCR.java
@@ -17,12 +17,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,21 +45,25 @@ import androidx.core.content.ContextCompat;
 public class VisualOCR extends AppCompatActivity {
 
     ImageView mPreviewIv;
-
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String UID = user.getUid();
     private Button cameraBtn;
     private Button galleryBtn;
     private Button verifyAllergiesBtn;
     private Uri image_uri;
     private TextView resultTv;
     private TextView txtViewTop;
+    private TextView verdictTv;
 
     private static final int CAMERA_REQUEST_CODE = 200;
     private static final int STORAGE_REQUEST_CODE = 400;
     private static final int IMAGE_PICK_GALLERY_CODE = 1000;
     private static final int IMAGE_PICK_CAMERA_CODE = 1001;
-
+    List<String> allSynonyms = new ArrayList<>();
     String cameraPermission[];
     String storagePermission[];
+
+    StringBuilder sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,19 +80,11 @@ public class VisualOCR extends AppCompatActivity {
         resultTv.setVisibility(View.GONE);
         verifyAllergiesBtn = findViewById(R.id.viewAllergiesBtn);
         verifyAllergiesBtn.setVisibility(View.GONE);
-        verifyAllergiesBtn.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                verifyAllergies();
-            }
-        });
-
         mPreviewIv = findViewById(R.id.imageIv);
+        verdictTv = findViewById(R.id.verdictTv);
 
         cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
     }
 
 
@@ -100,9 +107,105 @@ public class VisualOCR extends AppCompatActivity {
     }
 
     public void onVerifyAllergiesBtnClicked(View view){
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+        TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
+        if (!recognizer.isOperational()){
+
+        }
+        else { // Display text from image
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = recognizer.detect(frame);
+            sb = new StringBuilder();
+
+            for (int i =0; i < items.size(); i ++){
+                TextBlock myItem = items.valueAt(i);
+                sb.append(myItem.getValue());
+                sb.append("\n");
+            }
+            resultTv.setVisibility(View.VISIBLE);
+            resultTv.setText(sb);
+        }
+
+        (FirebaseDatabase.getInstance().getReference()).child("users").child(UID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(snapshot == null || snapshot.getValue() == null)
+                    return;
+                String stringOfAllergies = snapshot.getValue().toString();
+                List<String> allergies = convertStringOfAllergiesIntoArray(stringOfAllergies);
+                String allergy = compareWithIngredients(allergies);
+                if(allergy.isEmpty()) {
+                    getVerdictSynonyms(allergies);
+                    return;
+                }
+
+                // Can conclude it is not safe
+                verdictTv.setText("Oh helllll nah, put it back! This contains " + allergy + ".");
+                resultTv.setVisibility(View.GONE);
+                verifyAllergiesBtn.setVisibility(View.GONE);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
+    public String compareWithIngredients(List<String> allergies) {
+        String allergy = "";
+        for(String a: allergies) {
+            if(sb.toString().contains(a.toLowerCase()) || sb.toString().contains(a) || sb.toString().contains(a.toUpperCase()))
+                allergy = a;
+        }
+
+        return allergy;
+    }
+
+    private List<String> getVerdictSynonyms(List<String> allergies) {
+
+        for(String a: allergies) {
+            (FirebaseDatabase.getInstance().getReference()).child("synonym").child(a.toLowerCase()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if(snapshot == null || snapshot.getValue() == null)
+                        return;
+                    String stringOfSynonyms = snapshot.getValue().toString();
+                    List<String> synonyms = convertStringOfAllergiesIntoArray(stringOfSynonyms);
+
+                    String allergy = "";
+                    for(String s: synonyms) {
+                        if(sb.toString().contains(s.toLowerCase()) || sb.toString().contains(s) || sb.toString().contains(s.toUpperCase()))
+                            allergy = s;
+                    }
+
+                    if(allergy.isEmpty()) {
+                        verdictTv.setText("No worries, you're good to go!");
+                    }
+                    else {
+                        verdictTv.setText("Oh helllll nah, put it back! This contains"  + allergy + ".");
+                    }
+
+                    mPreviewIv.setVisibility(View.GONE);
+                    verifyAllergiesBtn.setVisibility(View.GONE);
+                    resultTv.setVisibility(View.GONE);
+
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
+
+        return allSynonyms;
+    }
+
+    private List<String> convertStringOfAllergiesIntoArray(String stringOfAllergies) {
+        String[] allergiesArr = stringOfAllergies.split(",");
+        List<String> allergies = new ArrayList<>(Arrays.asList(allergiesArr));
+
+        return allergies;
+    }
 
     private boolean checkCameraPermission() {
         boolean resultCam  = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
@@ -141,8 +244,6 @@ public class VisualOCR extends AppCompatActivity {
 
 
     // handle permission result
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch(requestCode){
@@ -175,8 +276,6 @@ public class VisualOCR extends AppCompatActivity {
     }
 
     //handleImageResult
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -215,28 +314,7 @@ public class VisualOCR extends AppCompatActivity {
             }
         }
     }
-    public void verifyAllergies(){
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
-        Bitmap bitmap = bitmapDrawable.getBitmap();
-        TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
-        if (!recognizer.isOperational()){
-
-        }
-        else{
-            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-            SparseArray<TextBlock> items = recognizer.detect(frame);
-            StringBuilder sb = new StringBuilder();
-
-            for (int i =0; i < items.size(); i ++){
-                TextBlock myItem = items.valueAt(i);
-                sb.append(myItem.getValue());
-                sb.append("\n");
-            }
-            resultTv.setVisibility(View.VISIBLE);
-            resultTv.setText(sb);
-        }
-    }
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()) {
             case android.R.id.home:
